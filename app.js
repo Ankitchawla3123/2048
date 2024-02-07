ab1db465f63bcea54c7ddfc950a30bfd9c3b28be
SPYD3123 <ankitchawla3123@gmail.com>
2024-02-07 20:32:54 +0530

    grid, css and left right top and bottom
    js logic added for left right top nd bottom
    grid created with tile class also using html and css

--- app.js
+++ app.js
@@ -1,59 +1,88 @@
-function set_board() {
-    let arr = [
-        [2, 4, 4, 0],
-        [0, 2, 0, 2],
-        [0, 2, 0, 2],
-        [2, 2, 2, 2],
-    ];
+// let div1=document.createElement("div");
+// let div2=document.createElement("div");
+// a.appendChild(div1);
+// a.appendChild(div2);
+// div1.classList.add("tile");
+// div2.classList.add("tile");
+// div1.style.setProperty("--y",0);
+let grid = document.querySelector(".container");
+let board = [[0, 2, 2, 2],
+[0, 2, 2, 0],
+[2, 0, 0, 4],
+[2, 2, 2, 2]];
 
-    // generaterandomstarts(arr);
-    move(arr);
-    console.log(arr);
 
-}
+function onleft() {
+    for (let r = 0; r < board.length; r++) {
+        let a = board[r].slice(); // Copy the array elements to avoid referencing the same array object
+        let i = 0;
+        let j = 1;
+        while (j < a.length) {
+            if (a[i] === 0 && a[j] !== 0) {
+                a[i] = a[j];
+                a[j] = 0;
+                i++;
+            }
+            if (a[i]!=0) {
+                i++;
+            }
+            j++;
+        }
+        let n = 1;
+        while (n < a.length) {
+            if (a[n - 1] === a[n]) {
+                a[n - 1] = a[n] * 2;
+                a[n] = 0;
+            }
+            n++;
+        }
+        console.log(a);
+        let extra=[];
+        for (let i = 0; i < a.length; i++) {
+            if (a[i]!=0) {
+                extra.push(a[i]);
+            }
+        }
+        let b=extra.length;
+        while (b!=4) {
+            extra.push(0);
+            b++;            
+        }
 
-function generaterandomstarts(arr) {
-    let row1 = Math.floor(Math.random() * 4);
-    let col1 = Math.floor(Math.random() * 4);
-    let row = Math.floor(Math.random() * 4);
-    let col = Math.floor(Math.random() * 4);
-    while (row1 == row && col1== col ) {
-        row = Math.floor(Math.random() * 4);
-        col = Math.floor(Math.random() * 4);
+        board[r] = extra; // Update the board with the modified row
     }
-
-
-    arr[row1][col1] = 2;
-    arr[row][col] = 2;
+    // console.log(board);
 }
-function move(arr){
-    let left=true;
-    if(left){
-        for (let i = 0; i < arr.length; i++) {
-            let clearzeros=[];
-            for (let j= 0; j < arr[i].length; j++) {
-                if (arr[i][j]!=0) {
-                    clearzeros.push(arr[i][j]);
-                }
-            }
-            console.log(clearzeros);
-            for (let k = 0; k< clearzeros.length; k++) {
-                if (k+1!=clearzeros.length) {
-                    if (clearzeros[k]==clearzeros[k+1]) {
-                        clearzeros[k]=2*clearzeros[k];
-                        clearzeros[k+1]=0;
-                    }
-                }
-            }
-            let finalrow = clearzeros.filter((x) => x !== 0);
 
-            while(finalrow.length!=4){
-                finalrow.push(0);
-            }
-            arr[i]=finalrow;
+function onright(){
+    onleft();
+    for (let i = 0; i < board.length; i++) {
+        board[i]=board[i].reverse();
+    }
+    // console.log(board);
+}
+
+function transpose(){
+    let arr=[];
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j<i; j++) {
+            const temp=board[i][j];
+            board[i][j]=board[j][i];
+            board[j][i]=temp;
         }
     }
 }
+function ontop(){
+    transpose();
+    onleft();
+    transpose();
+    console.log(board)
+}
+function onbottom(){
+    transpose();
+    onright();
+    transpose();
+    console.log(board);
+}
 
-
-set_board();
+// let finalrow = clearzeros.filter((x) => x !== 0);
